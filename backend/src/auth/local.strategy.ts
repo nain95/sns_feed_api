810c5423f8d3e7a499b37847989c067496723935
in_yeop <najio95@gmail.com>
2022-05-06 21:11:03 +0900

    feat: 페이스북 로그인 구현

---
+++ backend/src/auth/local.strategy.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Profile, Strategy } from "passport-facebook";
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, "facebook") {
+  constructor() {
+    super({
+      clientID: process.env.APP_ID,
+      clientSecret: process.env.APP_SECRET,
+      callbackURL: "https://c2c7-121-135-254-216.ngrok.io/auth/facebook/redirect",
+      scope: ["email", "user_posts"],
+      profileFields: ["email", "name"],
+    });
+  }
+  
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void
+  ): Promise<any> {
+    const { name, emails } = profile;
+    const user = {
+      email: emails,
+      firstName: name.givenName,
+      lastName: name.familyName,
+    };
+    const payload = {
+      user,
+      accessToken,
+    };
+    return done(null, payload);
+  }
+}
